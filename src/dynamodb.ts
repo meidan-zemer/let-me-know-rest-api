4bccfd3e44b4fc1a80e635767ab9c8f242005c5a
meidan.zemer <meidan@gmail.com>
2019-01-06 17:45:46 +0200

    Add update cp

--- src/dynamodb.ts
+++ src/dynamodb.ts
@@ -3,29 +3,30 @@ import AWS from "aws-sdk";
 import moment from "moment";
 import { DynamoDB } from "aws-sdk";
 import { contactPoint } from "let-me-know-ts-definitions";
-const dynamoDb = new AWS.DynamoDB.DocumentClient({ convertEmptyValues: true });
 
+const dynamoDb = new AWS.DynamoDB.DocumentClient({ convertEmptyValues: true });
 const contactPointTableName = "ContactPoints";
 const contactPointTableIndex = "cpId-index";
 
 export function getAllContactPoints(userId: string): Promise<contactPoint[]> {
   return new Promise<contactPoint[]>((resolve, reject) => {
-    const params: DynamoDB.DocumentClient.QueryInput= {
+    const params: DynamoDB.DocumentClient.QueryInput = {
       TableName: contactPointTableName,
       KeyConditionExpression: "#userId = :userId",
       ExpressionAttributeNames: { "#userId": "userId" },
-      ExpressionAttributeValues: { ":userId": userId}
+      ExpressionAttributeValues: { ":userId": userId }
     };
 
     dynamoDb
       .query(params)
       .promise()
-      .then((data: DynamoDB.QueryOutput) =>resolve(data.Items as contactPoint[]))
+      .then((data: DynamoDB.QueryOutput) =>
+        resolve(data.Items as contactPoint[])
+      )
       .catch(err => reject(err));
   });
 }
-
-export function putContcatPoint(
+export function putContactPoint(
   userId: string,
   name: string = "",
   description: string = ""
@@ -53,4 +54,81 @@ export function putContcatPoint(
       .then((data: DynamoDB.PutItemOutput) => resolve(cp))
       .catch((err: any) => reject(err));
   });
+}
+
+export function updateContactPoint(
+  userId: string,
+  cpId: string,
+  cp: contactPoint
+): Promise<contactPoint> {
+  return new Promise<contactPoint>((resolve, reject) => {
+    const now = moment()
+      .utc()
+      .valueOf();
+
+    let params: DynamoDB.DocumentClient.UpdateItemInput = {
+      TableName: contactPointTableName,
+      ReturnValues: "ALL_NEW",
+      ConditionExpression: "attribute_exists(cpId)",
+      Key: { cpId: cpId, userId }
+    };
+
+    params = addAttributesToUpdate(params, {"name":cp.name,"description":cp.description,"modifyDate":now});
+    dynamoDb
+      .update(params)
+      .promise()
+      .then((data: DynamoDB.UpdateItemOutput) => {
+        return getContactPoint(cpId);
+      })
+      .then(cp => resolve(cp))
+      .catch((err: any) => reject(err));
+  });
+}
+
+export function getContactPoint(cpId: string): Promise<contactPoint> {
+  return new Promise<contactPoint>((resolve, reject) => {
+    const params: DynamoDB.DocumentClient.QueryInput = {
+      TableName: contactPointTableName,
+      IndexName: contactPointTableIndex,
+      KeyConditionExpression: "#cpId = :cpId",
+      ExpressionAttributeNames: { "#cpId": "cpId" },
+      ExpressionAttributeValues: { ":cpId": cpId }
+    };
+
+    dynamoDb
+        .query(params)
+        .promise()
+        .then((data: DynamoDB.QueryOutput) =>{
+          if(data.Count && data.Items){
+            resolve(data.Items[0] as contactPoint);
+          } else {
+            reject();
+          }
+        })
+        .catch(err => reject(err));
+  });
+}
+
+function addAttributesToUpdate(
+    params: DynamoDB.DocumentClient.UpdateItemInput,
+    attributes:{ [key: string]: any}
+): DynamoDB.DocumentClient.UpdateItemInput {
+    if(attributes) {
+      params.ExpressionAttributeNames = {};
+      params.ExpressionAttributeValues =  {};
+      params.UpdateExpression = "";
+      for(const attributeName in attributes){
+        if(attributes.hasOwnProperty(attributeName)){
+          const attributeValue = attributes[attributeName];
+          if(attributeValue){
+            params.ExpressionAttributeNames["#" + attributeName] = attributeName;
+            params.ExpressionAttributeValues[":" + attributeName] = attributeValue;
+            let updateExpressionPrefix = ",";
+            if(params.UpdateExpression === "") updateExpressionPrefix = "SET "
+            params.UpdateExpression += updateExpressionPrefix + "#" + attributeName + "=" + ":" + attributeName;
+          }
+        }
+      }
+    }
+  return params;
 }
--- src/handler.ts
+++ src/handler.ts
@@ -2,20 +2,23 @@
 import express from "express";
 import serverlessExpress from "aws-serverless-express";
 import awsServerlessExpressMiddleware from "aws-serverless-express/middleware";
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
 import { Request } from "express-serve-static-core";
-import { getAllContactPoints, putContcatPoint } from "./dynamodb";
+import {
+  getAllContactPoints,
+  putContactPoint,
+  getContactPoint,
+  updateContactPoint
+} from "./dynamodb";
 import { contactPoint } from "let-me-know-ts-definitions";
 
 const app = express();
-
 
 app.use(awsServerlessExpressMiddleware.eventContext());
 app.use(bodyParser.json()); //parse application/json
 app.get("/rest/contactPoints", (req: Request | any, res) => {
   const event = req.apiGateway.event;
   const userId = getUserId(event);
-  console.log("userID:"+userId);
   getAllContactPoints(userId)
     .then((items: contactPoint[]) => {
       res.send(items);
@@ -25,12 +28,35 @@ app.get("/rest/contactPoints", (req: Request | any, res) => {
       res.status(500).send(err);
     });
 });
-
+app.get("/rest/contactPoint/:cpId", (req: Request | any, res) => {
+  const cpId = req.params.cpId;
+  getContactPoint(cpId)
+    .then((cp: contactPoint) => {
+      res.send(cp);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send(err);
+    });
+});
 app.put("/rest/contactPoint", (req: Request | any, res) => {
   const event = req.apiGateway.event;
   const userId = getUserId(event);
   const cp: contactPoint = req.body;
-  putContcatPoint(userId, cp.name, cp.description)
+  putContactPoint(userId, cp.name, cp.description)
+    .then((item: contactPoint) => {
+      res.send(item);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
+app.post("/rest/contactPoint/:cpId", (req: Request | any, res) => {
+  const event = req.apiGateway.event;
+  const userId = getUserId(event);
+  const cp: contactPoint = req.body;
+  const cpId = req.params.cpId;
+  updateContactPoint(userId, cpId, cp)
     .then((item: contactPoint) => {
       res.send(item);
     })
@@ -40,22 +66,24 @@ app.put("/rest/contactPoint", (req: Request | any, res) => {
 });
 
 app.get("*", (req, res) => {
-  console.log("Got to 404:"+req.originalUrl);
-  res.status(404).send("Not found "+req.originalUrl);
+  res.status(404).send("Not found " + req.originalUrl);
+});
+app.put("*", (req, res) => {
+  res.status(404).send("Not found " + req.originalUrl);
+});
+app.post("*", (req, res) => {
+  res.status(404).send("Not found " + req.originalUrl);
 });
 
-
-function getUserId(event:any) : string {
-  let userId = null;
-  try{
-    userId = event.requestContext.authorizer.claims.sub;
-  }
-  catch(err){
-
-  }
+function getUserId(event: any): string {
+  let userId = event.requestContext.authorizer.claims.sub;
   return userId;
 }
 
-
 const server = serverlessExpress.createServer(app);
-module.exports.letMeKnow = (event: any, context: any) => serverlessExpress.proxy(server, event, context);
+module.exports.letMeKnow = (event: any, context: any) => {
+  console.log(JSON.stringify(event));
+  console.log(JSON.stringify(context));
+  return serverlessExpress.proxy(server, event, context);
+
+};
