e59c4b9f616c703f865253708dbf5b92449bc613
meidan.zemer <meidan@gmail.com>
2019-01-15 15:23:52 +0200

    refactor DB structure

--- src/dynamodb.ts
+++ src/dynamodb.ts
@@ -5,29 +5,57 @@ import { DynamoDB } from "aws-sdk";
 import { contactPoint } from "let-me-know-ts-definitions";
 import * as data from "../config.json";
 
+enum contactPointRoles {
+  None,
+  Owner
+}
 const config = data as any;
 const env = process.env.NODE_ENV;
 const dynamoDb = new AWS.DynamoDB.DocumentClient({ convertEmptyValues: true });
-const contacpPointIndexName =
-  config.dynamoDb.tables.ContactPoints.globalIndexName;
-const contacpPointTableName =
-    config.dynamoDb.tables.ContactPoints.tableName + "-" + env;
+
+const contacpPointsTableName =
+  config.dynamoDb.tables.ContactPoints.tableName + "-" + env;
+
+const contactPointsUsersTableName =
+  config.dynamoDb.tables.ContactPointsUsers.tableName + "-" + env;
 
 export function getAllContactPoints(userId: string): Promise<contactPoint[]> {
   return new Promise<contactPoint[]>((resolve, reject) => {
     const params: DynamoDB.DocumentClient.QueryInput = {
-      TableName: contacpPointTableName,
+      TableName: contactPointsUsersTableName,
       KeyConditionExpression: "#userId = :userId",
       ExpressionAttributeNames: { "#userId": "userId" },
       ExpressionAttributeValues: { ":userId": userId }
     };
-
     dynamoDb
       .query(params)
       .promise()
-      .then((data: DynamoDB.QueryOutput) =>
-        resolve(data.Items as contactPoint[])
-      )
+      .then((data: DynamoDB.DocumentClient.QueryOutput) => {
+        const cpIds: string[] = data.Items ? data.Items.map(cp => cp.cpId) : [];
+        const keysArray = cpIds.map(cpId => {
+          return {
+            [config.dynamoDb.tables.ContactPoints.fields.cpId]: cpId
+          };
+        });
+        keysArray.push({
+          [config.dynamoDb.tables.ContactPoints.fields.cpId]: "Dummy"
+        });
+        let params: DynamoDB.DocumentClient.BatchGetItemInput = {
+          RequestItems: {
+            [contacpPointsTableName]: {
+              Keys: keysArray
+            }
+          }
+        };
+        return dynamoDb.batchGet(params).promise();
+      })
+      .then(data => {
+        let cpIdsArr: any[] = [];
+        if (data && data.Responses && data.Responses[contacpPointsTableName]) {
+          cpIdsArr = data.Responses[contacpPointsTableName];
+        }
+        resolve(cpIdsArr as contactPoint[]);
+      })
       .catch(err => reject(err));
   });
 }
@@ -48,16 +76,32 @@ export function putContactPoint(
       createDate: now,
       modifyDate: now
     };
-    const params: DynamoDB.DocumentClient.PutItemInput = {
-      TableName: contacpPointTableName,
+    const putCpIdparams: DynamoDB.DocumentClient.Put = {
+      TableName: contacpPointsTableName,
       Item: cp as any
     };
 
+    const putCpIdUserparams: DynamoDB.DocumentClient.Put = {
+      TableName: contactPointsUsersTableName,
+      Item: {
+        cpId: cp.cpId,
+        userId: cp.userId,
+        role: contactPointRoles.Owner
+      }
+    };
+    const writeTransPara: DynamoDB.DocumentClient.TransactWriteItemsInput = {
+      TransactItems: [{ Put: putCpIdparams }, { Put: putCpIdUserparams }]
+    };
+
     dynamoDb
-      .put(params)
+      .transactWrite(writeTransPara)
       .promise()
-      .then((data: DynamoDB.PutItemOutput) => resolve(cp))
-      .catch((err: any) => reject(err));
+      .then((data: DynamoDB.DocumentClient.TransactWriteItemsOutput) => {
+        resolve(cp);
+      })
+      .catch((err: any) => {
+        reject(err);
+      });
   });
 }
 
@@ -67,49 +111,49 @@ export function updateContactPoint(
   cp: contactPoint
 ): Promise<contactPoint> {
   return new Promise<contactPoint>((resolve, reject) => {
-    const now = moment()
-      .utc()
-      .valueOf();
+    getCpRolePerId(cpId, uid)
+      .then((role: contactPointRoles) => {
+        if (role !== contactPointRoles.Owner) {
+          throw "No Permissions";
+        }
+        const now = moment()
+          .utc()
+          .valueOf();
 
-    let params: DynamoDB.DocumentClient.UpdateItemInput = {
-      TableName: contacpPointTableName,
-      ReturnValues: "ALL_NEW",
-      ConditionExpression: "attribute_exists(cpId)",
-      Key: { cpId: cpId, userId: uid }
-    };
+        let params: DynamoDB.DocumentClient.UpdateItemInput = {
+          TableName: contacpPointsTableName,
+          ReturnValues: "ALL_NEW",
+          ConditionExpression: "attribute_exists(cpId)",
+          Key: { cpId: cpId }
+        };
 
-    params = addAttributesToUpdate(params, {
-      name: cp.name,
-      description: cp.description,
-      modifyDate: now
-    });
-    dynamoDb
-      .update(params)
-      .promise()
-      .then((data: DynamoDB.UpdateItemOutput) => {
-        return getContactPoint(cpId);
+        params = addAttributesToUpdate(params, {
+          name: cp.name,
+          description: cp.description,
+          modifyDate: now
+        });
+        return dynamoDb.update(params).promise();
       })
+      .then((data: DynamoDB.UpdateItemOutput) => getContactPoint(cpId))
       .then(cp => resolve(cp))
-      .catch((err: any) => reject(err));
+      .catch((err: any) => {
+        reject(err);
+      });
   });
 }
 
 export function getContactPoint(cpId: string): Promise<contactPoint> {
   return new Promise<contactPoint>((resolve, reject) => {
-    const params: DynamoDB.DocumentClient.QueryInput = {
-      TableName: contacpPointTableName,
-      IndexName: contacpPointIndexName,
-      KeyConditionExpression: "#cpId = :cpId",
-      ExpressionAttributeNames: { "#cpId": "cpId" },
-      ExpressionAttributeValues: { ":cpId": cpId }
+    const params: DynamoDB.DocumentClient.GetItemInput = {
+      TableName: contacpPointsTableName,
+      Key: { cpId: cpId }
     };
-
     dynamoDb
-      .query(params)
+      .get(params)
       .promise()
-      .then((data: DynamoDB.QueryOutput) => {
-        if (data.Count && data.Items) {
-          resolve(data.Items[0] as contactPoint);
+      .then((data: DynamoDB.DocumentClient.GetItemOutput) => {
+        if (data.Item) {
+          resolve(data.Item as contactPoint);
         } else {
           reject();
         }
@@ -148,4 +192,24 @@ function addAttributesToUpdate(
     }
   }
   return params;
+}
+
+function getCpRolePerId(cpId: string, userId: string): Promise<number> {
+  return new Promise((resolve, reject) => {
+    const params: DynamoDB.DocumentClient.GetItemInput = {
+      TableName: contactPointsUsersTableName,
+      Key: {
+        cpId: cpId,
+        userId: userId
+      }
+    };
+    dynamoDb
+      .get(params)
+      .promise()
+      .then((data: DynamoDB.DocumentClient.GetItemOutput) => {
+        let role = data.Item ? data.Item.role : contactPointRoles.None;
+        resolve(role);
+      })
+      .catch(err => reject(err));
+  });
 }
--- src/webHandler.ts
+++ src/webHandler.ts
@@ -1,18 +1,18 @@
 "use strict";
 import express from "express";
-import uuid from 'uuid';
-import cookieParser from 'cookie-parser';
+import uuid from "uuid";
+import cookieParser from "cookie-parser";
 import serverlessExpress from "aws-serverless-express";
 import awsServerlessExpressMiddleware from "aws-serverless-express/middleware";
 import bodyParser from "body-parser";
 import { Request } from "express-serve-static-core";
 import {
-    getAllContactPoints,
-    putContactPoint,
-    getContactPoint,
-    updateContactPoint
+  getAllContactPoints,
+  putContactPoint,
+  getContactPoint,
+  updateContactPoint
 } from "./dynamodb";
-import {  discussion,message} from "let-me-know-ts-definitions";
+import { discussion, message } from "let-me-know-ts-definitions";
 
 const app = express();
 
@@ -20,24 +20,39 @@ app.use(awsServerlessExpressMiddleware.eventContext());
 app.use(bodyParser.json()); //parse application/json
 app.use(cookieParser());
 
-app.get("/rest/discussion/:cpid", (req: Request | any, res) => {
-    res.send(req.cookies)
-});
-app.put("/rest/discussion/:cpid", (req: Request | any, res) => {
-    res.cookie("clientId", uuid(),{maxAge:1000*10});
-    res.send("OK");
+app.get("/rest/discussion/:discussionId", (req: Request | any, res) => {
+  res.send(req.cookies);
 });
 
+app.put("/rest/discussion/:discussionId", (req: Request | any, res) => {
+  res.cookie("clientId", uuid(), { maxAge: 1000 * 10 });
+  res.send("OK");
+});
+
+app.get(
+  "/rest/discussion/:discussionId/messages",
+  (req: Request | any, res) => {
+    res.send(req.cookies);
+  }
+);
+app.put(
+  "/rest/discussion/:discussionId/messages",
+  (req: Request | any, res) => {
+    res.cookie("clientId", uuid(), { maxAge: 1000 * 10 });
+    res.send("OK");
+  }
+);
+
 app.get("*", (req, res) => {
-    res.status(404).send("Not found " + req.originalUrl);
+  res.status(404).send("Not found " + req.originalUrl);
 });
 app.put("*", (req, res) => {
-    res.status(404).send("Not found " + req.originalUrl);
+  res.status(404).send("Not found " + req.originalUrl);
 });
 
 const server = serverlessExpress.createServer(app);
 module.exports.letMeKnow = (event: any, context: any) => {
-    console.log(JSON.stringify(event));
-    console.log(JSON.stringify(context));
-    return serverlessExpress.proxy(server, event, context);
+  console.log(JSON.stringify(event));
+  console.log(JSON.stringify(context));
+  return serverlessExpress.proxy(server, event, context);
 };
